03e2c3a5094564208391222e0b464edac5a4ad1b
Mohit <mohit.kumar@zconsolutions.com>
2020-07-13 20:46:12 +0530

    changed port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ App.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-App.listen(5000, () => {
-  console.log('running on 3000');
+App.listen(PORT, () => {
+  console.log('running on ', PORT);
 });
