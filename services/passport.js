c35032c0bae5c15ba45e9b899538a64595c22812
Mohit <mohit.kumar@zconsolutions.com>
2020-07-14 17:16:07 +0530

    solving redirect uri

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+  User.findById(id).then((user) => {
     done(null, user);
   });
 });
@@ -20,10 +20,10 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: `${keys.googleRedirectURI}/auth/google/callback`
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           // we already have a record with the given profile ID
           done(null, existingUser);
@@ -31,7 +31,7 @@ passport.use(
           // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
-            .then(user => done(null, user));
+            .then((user) => done(null, user));
         }
       });
     }
